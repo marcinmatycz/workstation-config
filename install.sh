6d77625642c57c558fdfa4782debbc73881bb64b
Marcin Matycz <matyczmarcin@gmail.com>
2024-06-02 13:31:51 +0200

    Added going on with the install script if packer.nvim is already cloned.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim || true
 cp -r config/* ~/.config
 
